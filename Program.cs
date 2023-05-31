3dfe94fd9f495e2878d274758fe14587c82263cd
Albert Delgado <bolshoi19booze@gmail.com>
2023-05-30 22:17:51 -0400

    Limpiando

--- Program.cs
+++ Program.cs
@@ -25,7 +25,6 @@ namespace GeekEngineer
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            builder.Services.AddSingleton<WeatherForecastService>();
 
             var app = builder.Build();
 
