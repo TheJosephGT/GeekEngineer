85a1efa4d2015ac3eac40cfd849119e7ff42094f
Jorge Toribio <jorgeluistoribio14@gmail.com>
2023-06-01 15:38:28 -0400

    Registros planteados

---
+++ Models/Proveedores.cs
@@ -0,0 +1,15 @@
+
+public class Proveedores
+{
+    [Key]
+    public int ProveedorId { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string NombreEmpresa { get; set; } = string.Empty;
+    public string RNCEmpresa { get; set; } = string.Empty;
+    public string NCFEmpresa { get; set; } = string.Empty;
+    public DateOnly Fecha { get; set; }
+    public int ProductoId { get; set; }
+    public string Direccion { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
+}
