fc42bf40f7fe13602baf2e8118a4afc4657c3499
Albert Delgado <bolshoi19booze@gmail.com>
2023-06-06 16:06:31 -0400

    Update

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -8,8 +8,11 @@ public class Productos
     [DataType(DataType.Date)]
     public DateOnly Fecha { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     [Required]
+    [Range(minimum: 1, maximum: float.MaxValue, ErrorMessage = "Ingrese un costo mayor a 0.")]
+    public double Costo { get; set; }
+    [Required]
     [Range(minimum: 1, maximum: float.MaxValue, ErrorMessage = "Ingrese un precio mayor a 0.")]
-    public double PrecioCompra { get; set; }
+    public double Precio { get; set; }
     [Required(ErrorMessage = "Campo ITBIS es obligatorio.")]
     [Range(minimum: 1, maximum: float.MaxValue, ErrorMessage = "Seleccione el % de ITBIS.")]
     public double ITBIS { get; set; }
