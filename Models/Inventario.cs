c288bf88d667169264e6b401d0796d48d6f4ad5f
Jorge Toribio <jorgeluistoribio14@gmail.com>
2023-06-10 13:17:51 -0400

    categoria add

--- Models/Inventario.cs
+++ Models/Inventario.cs
@@ -1,6 +1,10 @@
 public class Inventario
 {
+    [Key]
     public int InventarioId { get; set; }
     public int ProductoId { get; set; }
-    public int Existencia { get; set; } 
+    public int Cantidad { get; set; }
+    public DateOnly Fecha { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    public int ProveedorId { get; set; }
+
 }
