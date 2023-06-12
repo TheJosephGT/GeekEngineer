f5a0e55588dc16c377ae4d399732c0037da8020c
Albert Delgado <bolshoi19booze@gmail.com>
2023-06-12 10:22:06 -0400

    Database update

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace GeekEngineer
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseSqlite(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
